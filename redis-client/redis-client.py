55a09bf659cc91ccfc1be7308e55e486ccc0d3bb
Suraj <suraj@localhost.localdomain>
2021-03-22 20:04:43 +0530

    added .github for github actions

---
+++ redis-client/redis-client.py
@@ -0,0 +1,54 @@
+import socket
+from flask import Flask, Response
+from redis import Redis
+
+app = Flask(__name__)
+
+hostname = str(socket.gethostname())
+
+
+class Connection(object):
+    _conn = None
+
+    @classmethod
+    def __new__(cls,):
+        if cls._conn is None:
+            cls._conn = Redis(host="localhost", port=6379, db=0)
+        return cls._conn
+
+
+@app.route('/')
+def index():
+    return Response(response="Working", status=200)
+
+
+@app.route('/healthz')
+def healthz():
+    rd = Connection()._conn
+    if rd.ping():
+        return Response(response="Redis is Working using client on "+hostname, status=200)
+
+
+@app.route('/incr')
+def incr():
+    rd = Connection()._conn
+    count = str(rd.incrby("key:count", 1))
+    return Response(response=count, status=200)
+
+
+@app.route('/decr')
+def decr():
+    rd = Connection()._conn
+    count = str(rd.decrby("key:count", 1))
+    return Response(response=count, status=200)
+
+
+@app.route('/reset')
+def reset():
+    rd = Connection()._conn
+    count = str(rd.set("key:count", 0))
+    return Response(response=count, status=200)
+
+
+if __name__ == '__main__':
+    app.run(port=8085)
